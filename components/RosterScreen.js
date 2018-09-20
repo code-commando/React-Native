c6565214330f785023fc57f88b8e927657186c9c
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-09-19 21:44:29 -0700

    pulled from updated master and added roster code to RosterScreen

--- components/RosterScreen.js
+++ components/RosterScreen.js
@@ -1,8 +1,26 @@
 import React,{Fragment} from 'react';
-import { View, Text,Button } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 //import Menu from './Menu.js'
+const API = 'http://api.commando.ccs.net/api/v1/roster';
+
 export default class RosterScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      student: '',
+      allStudents: [],
+  }
+}
+componentDidMount(){
+
+    fetch(API)
+    .then((res) => res.json())
+    .then((roster) => {
+      let allStudents = (roster.results);
+      this.setState({allStudents});
+          })
+}
     static navigationOptions = {
       title: 'Roster',
     };
@@ -10,7 +28,7 @@ export default class RosterScreen extends React.Component {
         return (
            
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Roster Component display here</Text>
+           <FlatList data={[...this.state.allStudents]} renderItem={({item}) => <Text style={styles.name}>{item}</Text>}/>
           </View>
          
           
@@ -20,3 +38,15 @@ export default class RosterScreen extends React.Component {
   RosterScreen.navigationOptions = {
     title: 'Roster',
   };
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    name: {
+      fontSize: 18,
+      textAlign: 'center',
+    }
+  });
