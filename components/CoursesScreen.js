3eb07dd0336253369f561fa75d886d31dfe6c86b
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-09-22 15:51:37 -0700

    added text input and button to CoursesScreen

--- components/CoursesScreen.js
+++ components/CoursesScreen.js
@@ -1,5 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  Picker, 
+  ScrollView, 
+} from 'react-native';
+import Container from './styles/Container';
+import Button from './styles/Button';
+import Label from './styles/Label';
+import DropdownMenu from 'react-native-dropdown-menu';
+
+export default class CoursesScreen extends Component {
+  constructor(props) {
+    super(props);
+    state = {
+      classCode: '',
+      courseCode: '401n5',
+      day: 1
+    }
+  }
+
+render() {
+  //let courseDay = [[1,2,3,4,5],[6,7,8,9,10]];
+
+  return(
+    <ScrollView style={styles.scroll}>
+      <Container>
+        <Text style={styles.welcome}>
+          Courses Screen
+        </Text>
+      </Container>
+    <Container>
+      <Label text="Enter Class Code" />
+      <TextInput style={styles.textInput} onChangeText={(classCode) => this.setState({classCode})}/>
+    </Container>
+    <Container>
+      <Button 
+        label="Enter"
+        styles={{button: styles.alignRight, label: styles.label}} 
+        onPress={this.press.bind(this)} />
+    </Container>
+
+    </ScrollView>
+  );
+}
+}
+CoursesScreen.navigationOptions = {
+  title: 'Courses',
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -8,6 +58,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  label: {
+    color: '#0d8898',
+    fontSize: 20
+  },
+  alignRight: {
+    alignSelf: 'flex-end'
+  },
+  scroll: {
+    backgroundColor: '#E1D7D8',
+    padding: 30,
+    flexDirection: 'column'
+  },
+  textInput: {
+    height: 80,
+    fontSize: 30,
+    backgroundColor: '#FFF',
+  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -15,16 +82,29 @@ const styles = StyleSheet.create({
   },
 });
 
-const CoursesScreen = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Courses Screen
-    </Text>
-  </View>
-);
 
-CoursesScreen.navigationOptions = {
-  title: 'Courses',
-};
 
-export default CoursesScreen;
+// const CoursesScreen = () => (
+//   <View style={styles.container}>
+//     <Text style={styles.welcome}>
+//       Courses Screen
+//     </Text>
+//   </View>
+// );
+// <Picker
+// selectedValue={this.state.day}
+// style={{ height: 50, width: 100 }}
+// onValueChange={(itemValue, i) => this.setState({day: courseDay[itemValue][i]})}>
+// <Picker.Item label="Day" value={courseDay[i] }/>
+
+// </Picker> 
+      /* <DropdownMenu 
+        style={{flex: 1}}
+        bgColor={'white'}
+        tintColor={'#666666'}
+        activityTintColor={'green'}
+        handler={(selection, row) => this.setState({day: courseDay[selection][row]})} courseDay={courseDay}>
+        <View style={{flex: 1}}>
+          <Text>Day: {this.state.day}</Text>
+        </View>
+      </DropdownMenu> */
